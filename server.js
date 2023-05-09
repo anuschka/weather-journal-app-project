32cecb07d31bf67c450422c7dff6da0b80850527
anuschka <sustic@gmail.com>
2023-05-09 18:55:15 +0200

    Sethup the backend logic.

--- server.js
+++ server.js
@@ -2,8 +2,12 @@
 projectData = {};
 
 // Require Express to run server and routes
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
@@ -11,9 +15,28 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+app.use(cors());
+
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
 
 // Setup Server
+const port = 8000;
+const server = app.listen(port, listening);
+
+function listening(){
+    console.log("server running");
+    console.log(`running on localhost: ${port}!`)
+}
+
+// Add a GET route at the /data endpoint that sends projectData object as a response
+app.get('/data', (req,res) => {
+    console.log(req.body);
+    projectData.temperature = req.body.temperature;
+    projectData.date = req.body.date;
+    projectData.userResponse = req.body.userResponse;
+    res.send(projectData);
+    
+});
